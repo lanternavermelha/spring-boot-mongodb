a220741c41924b043c9440897b89254715291d9d
lanternavermelha <f_figueira@hotmail.com>
2019-12-17 23:17:04 +0000

    Adding comments to posts

--- src/main/java/com/udemy/course/config/Instantiation.java
+++ src/main/java/com/udemy/course/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.udemy.course.domain.Post;
 import com.udemy.course.domain.User;
 import com.udemy.course.dto.AuthorDTO;
+import com.udemy.course.dto.CommentDTO;
 import com.udemy.course.repository.PostRepository;
 import com.udemy.course.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		p1.getComments().addAll(Arrays.asList(c1,c2));
+		p2.getComments().addAll(Arrays.asList(c3));
 		
 		PostRepository.saveAll(Arrays.asList(p1,p2));
 		
