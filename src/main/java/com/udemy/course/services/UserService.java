8eac7b42db443ae17b11f98a98f41f4ff0cf3918
lanternavermelha <f_figueira@hotmail.com>
2019-12-17 11:49:45 +0000

    Get user by id

--- src/main/java/com/udemy/course/services/UserService.java
+++ src/main/java/com/udemy/course/services/UserService.java
@@ -1,12 +1,14 @@
 package com.udemy.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.udemy.course.domain.User;
 import com.udemy.course.repository.UserRepository;
+import com.udemy.course.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException(id));
 	}
 }
