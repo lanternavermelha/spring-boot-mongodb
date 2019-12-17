c82e8a6eac9fea128c48041ccb68d7156cf25956
lanternavermelha <f_figueira@hotmail.com>
2019-12-17 22:32:06 +0000

    Entity Post created with nested User

--- src/main/java/com/udemy/course/config/Instantiation.java
+++ src/main/java/com/udemy/course/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.udemy.course.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.udemy.course.domain.Post;
 import com.udemy.course.domain.User;
+import com.udemy.course.repository.PostRepository;
 import com.udemy.course.repository.UserRepository;
 
 @Configuration
@@ -14,16 +18,28 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository PostRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		PostRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		
+		
+		PostRepository.saveAll(Arrays.asList(p1,p2));
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 	}
 
