a02a7213896a3dda9c5083401cecd95366e0710a
lanternavermelha <f_figueira@hotmail.com>
2019-12-17 22:41:22 +0000

    Author data projection using DTO

--- src/main/java/com/udemy/course/config/Instantiation.java
+++ src/main/java/com/udemy/course/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.udemy.course.domain.Post;
 import com.udemy.course.domain.User;
+import com.udemy.course.dto.AuthorDTO;
 import com.udemy.course.repository.PostRepository;
 import com.udemy.course.repository.UserRepository;
 
@@ -35,12 +36,13 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		PostRepository.saveAll(Arrays.asList(p1,p2));
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
 	}
 
 }
